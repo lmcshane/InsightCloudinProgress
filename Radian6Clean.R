c7d4e796bec3d9a7790d511be9c0951ac126041c
Lauren McShane <lauren.m.mcshane@gmail.com>
2014-10-17 11:47:57 -0700

    Host variable clean

--- Radian6Clean.R
+++ Radian6Clean.R
@@ -2,6 +2,7 @@ setwd("C:/Users/lmcshane/Documents/megabloks")
 data <- read.csv("megabloks_20140909_r6.csv", header=T)
 
 #R6 date conversion
-date<- paste(substr(data$PublishedDate, 6, 7), substr(data$PublishedDate, 9, 10), substr(data$PublishedDate, 1, 4), sep= '/') 
-date.dataframe <- as.data.frame(date)
-data.date <- cbind(date.dataframe, data)
+data$PublishedDate <- paste(substr(data$PublishedDate, 6, 7), substr(data$PublishedDate, 9, 10), substr(data$PublishedDate, 1, 4), sep= '/') 
+
+#Clean Host variable
+data$Host = gsub( "www.", "",data$Host)
