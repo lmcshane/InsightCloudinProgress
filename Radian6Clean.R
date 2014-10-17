3acda0e0c8b343c087db9fc6c2ee934daf6ece27
Lauren McShane <lauren.m.mcshane@gmail.com>
2014-10-17 12:21:00 -0700

    Convert media type id to media type

--- Radian6Clean.R
+++ Radian6Clean.R
@@ -6,3 +6,26 @@ data$PublishedDate <- paste(substr(data$PublishedDate, 6, 7), substr(data$Publis
 
 #Clean Host variable
 data$Host = gsub( "www.", "",data$Host)
+
+#Convert media type id <- media type 
+
+recoderFunc <- function(data, oldvalue, newvalue) {
+  
+  # convert any factors to characters
+  
+  if (is.factor(data))     data     <- as.character(data)
+  if (is.factor(oldvalue)) oldvalue <- as.character(oldvalue)
+  if (is.factor(newvalue)) newvalue <- as.character(newvalue)
+  
+  # create the return vector
+  
+  newvec <- data
+  
+  # put recoded values into the correct position in the return vector
+  
+  for (i in unique(oldvalue)) newvec[data == i] <- newvalue[oldvalue == i]
+  
+  newvec
+  
+}
+
